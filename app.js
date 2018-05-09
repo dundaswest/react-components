67e56e85691d63aefe3c1b6e6e3de97a4f0bc777
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-09 13:08:17 -0700

    first

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// TODO
+var App = (props) => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryListItem todo={['Apple']}/>
+  </div>
+);
+
+var Apple = () => (
+  <div>Apple</div>
+)
+var Orange = () => (
+  <div>Orange</div>
+)
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textWeight: this.state.done ? 'bold' : 'none'
+    };
+    return (
+      <li style={style} onHover={this.onListItemHover.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
